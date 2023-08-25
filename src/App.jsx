0d32aa2859b6e166239343140af0e931d8cda856
Shota Pailodze <55694002+shotapailodze@users.noreply.github.com>
2023-08-25 13:13:37 +0400

    contact page done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' exact Component={Home}/>
           <Route path='/menu' exact Component={Menu}/>
           <Route path='/about' exact Component={About}/>
+          <Route path='/contact' exact Component={Contact}/>
         </Routes>
         <Footer />
       </Router>
